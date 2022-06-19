f4e9b76e5bb6be2f2e4fcd92b0ff0c64e315264d
jessicaphan193 <46518709+jessicaphan193@users.noreply.github.com>
2022-06-19 02:49:10 -0700

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,1 +1,11 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://<username>:<password>@cluster0.izawcpw.mongodb.net/?retryWrites=true&w=majority";
+
+client = MongoClient(url)
+
+db = client.pytech
+
+print(db.list_collection_names)
+
 
