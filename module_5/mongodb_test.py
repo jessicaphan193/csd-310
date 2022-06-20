bdeee60b79db6ce16201e41aa49d9f78c220fde7
jessicaphan193 <j.phan2@students.clark.edu>
2022-06-19 18:46:08 -0700

    changes

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,11 +1,13 @@
 from pymongo import MongoClient
 
-url = "mongodb+srv://<username>:<password>@cluster0.izawcpw.mongodb.net/?retryWrites=true&w=majority";
+import pymongo
 
+url = "mongodb+srv://admin:admin@cluster0.izawcpw.mongodb.net/?retryWrites=true&w=majority";
+
+
+from pymongo import MongoClient
 client = MongoClient(url)
 
 db = client.pytech
 
-print(db.list_collection_names)
-
-
+print(db.list_collection_names())
--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,1 +1,15 @@
+from pymongo import MongoClient
+
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.izawcpw.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+db = client.test
+col = db['test1']
+
+dic = {"first_name": "Thorin"}
+
+x = col.insert_one(dic)
+
+print(x)
+
 
